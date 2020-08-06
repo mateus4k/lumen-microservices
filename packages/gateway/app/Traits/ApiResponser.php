3075686d50bb7f9ed5a1fb9832d2c67211560e08
mateus4k <mateus4k@protonmail.ch>
2020-08-06 20:27:36 -0300

    Adapt apiResponser to receive microservices responses

---
+++ packages/gateway/app/Traits/ApiResponser.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Traits;
+
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Response;
+
+trait ApiResponser
+{
+    /**
+     * @param $data
+     * @param int $code
+     * @return Response
+     */
+    public function successResponse($data, int $code = Response::HTTP_OK): Response
+    {
+        return response($data, $code)->header('Content-Type', 'application/json');
+    }
+
+    /**
+     * @param $data
+     * @param int $code
+     * @return JsonResponse
+     */
+    public function errorResponse($data, int $code = Response::HTTP_BAD_REQUEST): JsonResponse
+    {
+        return response()->json(['error' => $data, 'code' => $code], $code);
+    }
+
+    /**
+     * @param $message
+     * @param int $code
+     * @return Response
+     */
+    public function errorMessage($message, int $code = Response::HTTP_BAD_REQUEST): Response
+    {
+        return response($message, $code)->header('Content-Type', 'application/json');
+    }
+}
