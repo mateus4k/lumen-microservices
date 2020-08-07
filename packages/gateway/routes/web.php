8c6b2b55294a2bba13edf5c94fdb0c349b55b9fc
mateus4k <mateus4k@protonmail.ch>
2020-08-06 22:29:47 -0300

    Add check credentials middleware for internal gateway routes

--- packages/gateway/routes/web.php
+++ packages/gateway/routes/web.php
@@ -4,25 +4,26 @@ $router->get('/', function () use ($router) {
     return $router->app->version();
 });
 
-/**
- * Router for authors
- */
-$router->get('authors', 'AuthorController@index');
-$router->post('authors', 'AuthorController@store');
-$router->get('authors/{author}', 'AuthorController@show');
-$router->put('authors/{author}', 'AuthorController@update');
-$router->patch('authors/{author}', 'AuthorController@update');
-$router->delete('authors/{author}', 'AuthorController@destroy');
+$router->group(['middleware' => 'client.credentials'], function () use ($router) {
+    /**
+     * Router for authors
+     */
+    $router->get('authors', 'AuthorController@index');
+    $router->post('authors', 'AuthorController@store');
+    $router->get('authors/{author}', 'AuthorController@show');
+    $router->put('authors/{author}', 'AuthorController@update');
+    $router->patch('authors/{author}', 'AuthorController@update');
+    $router->delete('authors/{author}', 'AuthorController@destroy');
 
-/**
- * Router for books
- */
-$router->get('books', 'BookController@index');
-$router->post('books', 'BookController@store');
-$router->get('books/{book}', 'BookController@show');
-$router->put('books/{book}', 'BookController@update');
-$router->patch('books/{book}', 'BookController@update');
-$router->delete('books/{book}', 'BookController@destroy');
-
+    /**
+     * Router for books
+     */
+    $router->get('books', 'BookController@index');
+    $router->post('books', 'BookController@store');
+    $router->get('books/{book}', 'BookController@show');
+    $router->put('books/{book}', 'BookController@update');
+    $router->patch('books/{book}', 'BookController@update');
+    $router->delete('books/{book}', 'BookController@destroy');
+});
 
 
