ddab24a9919a29499624913af5badb0654c0e109
mateus4k <mateus4k@protonmail.ch>
2020-08-06 21:43:13 -0300

    Books CRUD

--- packages/gateway/app/Http/Controllers/BookController.php
+++ packages/gateway/app/Http/Controllers/BookController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Services\BookService;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class BookController extends Controller
 {
@@ -13,9 +15,28 @@ class BookController extends Controller
         $this->bookService = $bookService;
     }
 
-
     public function index()
     {
+        return $this->successResponse($this->bookService->obtainBooks());
+    }
 
+    public function store(Request $request)
+    {
+        return $this->successResponse($this->bookService->createBook($request->all()), Response::HTTP_CREATED);
+    }
+
+    public function show($book)
+    {
+        return $this->successResponse($this->bookService->obtainBook($book));
+    }
+
+    public function update(Request $request, $book)
+    {
+        return $this->successResponse($this->bookService->editBook($request->all(), $book));
+    }
+
+    public function destroy($book)
+    {
+        return $this->successResponse($this->bookService->deleteBook($book));
     }
 }
--- packages/gateway/app/Services/BookService.php
+++ packages/gateway/app/Services/BookService.php
@@ -3,6 +3,7 @@
 namespace App\Services;
 
 use App\Traits\ConsumesExternalService;
+use GuzzleHttp\Exception\GuzzleException;
 
 class BookService
 {
@@ -13,5 +14,60 @@ class BookService
     public function __construct()
     {
         $this->baseUri = config('services.books.base_uri');
+    }
+
+    /**
+     * Obtain the full list of book from the book service
+     * @return string
+     * @throws GuzzleException
+     */
+    public function obtainBooks()
+    {
+        return $this->performRequest('GET', '/books');
+    }
+
+    /**
+     * Create one book using the book service
+     * @param $data
+     * @return string
+     * @throws GuzzleException
+     */
+    public function createBook($data)
+    {
+        return $this->performRequest('POST', '/books', $data);
+    }
+
+    /**
+     * Obtain one single book from the book service
+     * @param $book
+     * @return string
+     * @throws GuzzleException
+     */
+    public function obtainBook($book)
+    {
+        return $this->performRequest('GET', "/books/${book}");
+    }
+
+    /**
+     * Update an instance of book using the book service
+     * @param $data
+     * @param $book
+     * @return string
+     * @throws GuzzleException
+     */
+    public function editBook($data, $book)
+    {
+        return $this->performRequest('PUT', "/books/${book}", $data);
+    }
+
+    /**
+     * Remove a single book using the book service
+     * @param $book
+     * @return string
+     * @throws GuzzleException
+     */
+    public function deleteBook($book)
+    {
+        return $this->performRequest('DELETE', "/books/${book}");
     }
 }
