03c42266f1f81092520c707eab79691e93e0b7df
mateus4k <mateus4k@protonmail.ch>
2020-08-09 17:15:59 -0300

    Users CRUD on gateway

---
+++ packages/gateway/app/Http/Controllers/UserController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        $users = User::all();
+
+        return $this->validResponse($users);
+    }
+
+    public function store(Request $request)
+    {
+        $rules = [
+            'name'     => 'required|max:255',
+            'email'    => 'required|email|unique:user,email',
+            'password' => 'required|min:8|confirmed',
+        ];
+
+        $this->validate($request, $rules);
+
+        $fields             = $request->only(['name', 'email', 'password']);
+        $fields['password'] = Hash::make($request->get('password'));
+
+        $user = User::create($fields);
+
+        return $this->validResponse($user, Response::HTTP_CREATED);
+    }
+
+    public function show($user)
+    {
+        $user = User::findOrFail($user);
+
+
+        return $this->validResponse($user);
+    }
+
+    public function update(Request $request, $user)
+    {
+        $rules = [
+            'name'     => 'max:255',
+            'email'    => 'email|unique:user,email,' . $user,
+            'password' => 'min:8|confirmed',
+        ];
+
+        $this->validate($request, $rules);
+
+        $user = User::findOrFail($user);
+
+        $user->fill($request->only(['name', 'email', 'password']));
+
+        if ($request->has('password')) {
+            $user->password = Hash::make($request->get('password'));
+        }
+
+        if ($user->isClean()) {
+            return $this->errorResponse('At least one value must change', Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+
+        $user->save();
+
+        return $this->validResponse($user);
+    }
+
+    public function destroy($user)
+    {
+        $user = User::findOrFail($user);
+
+        $user->delete();
+
+        return $this->validResponse($user);
+    }
+}
--- packages/gateway/app/Traits/ApiResponser.php
+++ packages/gateway/app/Traits/ApiResponser.php
@@ -22,6 +22,16 @@ trait ApiResponser
      * @param int $code
      * @return JsonResponse
      */
+    public function validResponse($data, int $code = Response::HTTP_OK): JsonResponse
+    {
+        return response()->json(['data' => $data], $code);
+    }
+
+    /**
+     * @param $data
+     * @param int $code
+     * @return JsonResponse
+     */
     public function errorResponse($data, int $code = Response::HTTP_BAD_REQUEST): JsonResponse
     {
         return response()->json(['error' => $data, 'code' => $code], $code);
